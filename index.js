faf86edc4295a24ddd52b5bc2ae8c63fb9b34066
snlucas <lucas.lambda.101@gmail.com>
2023-10-11 00:41:07 -0300

    Fix members wrap

--- index.js
+++ index.js
@@ -25,12 +25,13 @@ rl.question(`Name yout want to package (default: ${defaultPackageName}): `, (use
         aura_bundle=$(git diff --ignore-all-space --ignore-blank-lines --ignore-cr-at-eol $(git log --reverse --since="${sinceCommit}" | head -n 1 | cut -d " " -f 2) ^HEAD | grep .*\.js$ | cut -d / -f 6 | cut -d " " -f 1 | sort | uniq | xargs | sed 's/^/\t\t\t/';);
 
         echo "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n<Package xmlns=\"http://soap.sforce.com/2006/04/metadata\">";
+
         if [ -n "apex_classes" ]; then
-          echo "\t<types>\n\t\t<members>\n$(git diff --ignore-all-space --ignore-blank-lines --ignore-cr-at-eol $(git log --reverse --since="${sinceCommit}" | head -n 1 | cut -d " " -f 2) ^HEAD | grep .*\.cls$ | cut -d / -f 6 | cut -d " " -f 1 | sort | uniq | sed 's/^/\t\t\t/')\n\t\t</members>\n\t\t<name>ApexClass</name>\n\t</types>";
+          echo "\t<types>\n$(git diff --ignore-all-space --ignore-blank-lines --ignore-cr-at-eol $(git log --reverse --since="${sinceCommit}" | head -n 1 | cut -d " " -f 2) ^HEAD | grep .*\.cls$ | cut -d / -f 6 | cut -d " " -f 1 | sort | uniq | awk '{print "\t\t<members>" $0 "</members>"}';)\n\t\t<name>ApexClass</name>\n\t</types>";
         fi
         
         if [ -n "aura_bundle" ]; then
-          echo "\t<types>\n\t\t<members>\n$(git diff --ignore-all-space --ignore-blank-lines --ignore-cr-at-eol $(git log --reverse --since="${sinceCommit}" | head -n 1 | cut -d " " -f 2) ^HEAD | grep .*\.js$ | cut -d / -f 6 | cut -d " " -f 1 | sort | uniq | xargs | sed 's/^/\t\t\t/')\n\t\t</members>\n\t\t<name>AuraDefinitionBundle</name>\n\t</types>";
+            echo "\t<types>\n$(git diff --ignore-all-space --ignore-blank-lines --ignore-cr-at-eol $(git log --reverse --since="${sinceCommit}" | head -n 1 | cut -d " " -f 2) ^HEAD | grep .*\.js$ | cut -d / -f 6 | cut -d " " -f 1 | sort | uniq | xargs | tr ' ' '\n' | awk '{print "\t\t<members>" $0 "</members>"}';)\n\t\t<name>AuraDefinitionBundle</name>\n\t</types>";
         fi
 
         echo "\t<version>44.0</version>\n</Package>";
